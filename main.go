b94666f28bb48883ee4b51bf21e5807f4e7ba6e4
Volodymyr Galkin <vladgalkin@gmail.com>
2016-11-18 12:31:00 +0200

    Split 'main.go' code into 'main.go' an 'superserver' struct

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+const (
+	defaultConfigPath         = "superserver.toml"
+	defaultTerminationTimeout = time.Millisecond * 3000
+)
+
+func main() {
+	config := getConfig()
+	if len(config.Services) == 0 {
+		log.Printf("No services specified. Exiting.\n")
+		return
+	}
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	superserver := &superserver{}
+	superserver.Start(config)
+	<-sigChan
+	log.Printf("Stopping...\n")
+	superserver.Stop()
+	log.Printf("Stopped\n")
+}
+
+func getConfig() superserverConfig {
+	configPath := flag.String("f", defaultConfigPath, "Config file path")
+	terminationTimeout := flag.Duration("t", defaultTerminationTimeout, "Child services termination timeout")
+	limit := flag.Uint("limit", 0, "Maximum number of concurrently running processes that can be started")
+	flag.Parse()
+
+	log.Printf("Reading config from file %s\n", *configPath)
+	fileConfig, err := readConfigFromFile(*configPath)
+	if err != nil {
+		log.Fatalf("Error reading config: %v\n", err)
+	}
+
+	return superserverConfig{
+		serviceTerminationTimeout: *terminationTimeout,
+		limit:  *limit,
+		config: fileConfig,
+	}
+}
--- superserver.go
+++ superserver.go
@@ -1,71 +1,53 @@
 package main
 
 import (
-	"flag"
 	"log"
 	"net"
-	"os"
-	"os/signal"
 	"strconv"
-	"syscall"
 	"time"
 
 	"golang.org/x/sync/errgroup"
 )
 
-const (
-	DefaultConfigPath         = "superserver.toml"
-	ListenerTimeout           = time.Millisecond * 500
-	DefaultTerminationTimeout = time.Millisecond * 3000
-)
+const listenerTimeout = time.Millisecond * 500
 
-func main() {
-	configPath := flag.String("f", DefaultConfigPath, "Config file path")
-	terminationTimeout := flag.Duration("t", DefaultTerminationTimeout, "Child services termination timeout")
-	limit := flag.Uint("limit", 0, "Maximum number of concurrently running processes that can be started")
-	flag.Parse()
+type superserverConfig struct {
+	serviceTerminationTimeout time.Duration
+	limit                     uint
+	*config
+}
 
-	log.Printf("Reading config from file %s\n", *configPath)
-	config, err := readConfigFromFile(*configPath)
-	if err != nil {
-		log.Fatalf("Error reading config: %v\n", err)
-	}
+type superserver struct {
+	stopListening chan struct{}
+	listenerGroup errgroup.Group
+	supervisor    *Supervisor
+}
 
-	if len(config.Services) == 0 {
-		log.Printf("No services specified. Exiting.\n")
-		return
-	}
-
-	supervisor := NewSupervisor(SupervisorConfig{
-		serviceTerminationTimeout: *terminationTimeout,
-		limit: *limit,
+func (server *superserver) Start(config superserverConfig) {
+	server.stopListening = make(chan struct{})
+	server.listenerGroup = errgroup.Group{}
+	server.supervisor = NewSupervisor(SupervisorConfig{
+		serviceTerminationTimeout: config.serviceTerminationTimeout,
+		limit: config.limit,
 	})
-	stopListening := make(chan struct{})
-	listenerGroup := errgroup.Group{}
 
 	for _, cfg := range config.Services {
 		serviceConfig := cfg
-		listenerGroup.Go(func() error {
-			listen(supervisor, serviceConfig, stopListening)
+		server.listenerGroup.Go(func() error {
+			server.listenForServiceConnections(serviceConfig)
 
 			return nil
 		})
 	}
-
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-
-	<-sigChan
-	log.Printf("Stopping...\n")
-
-	close(stopListening)
-	listenerGroup.Wait()
-
-	<-supervisor.Exit()
-	log.Printf("Stopped\n")
 }
 
-func listen(supervisor *Supervisor, config serviceConfig, stopListening <-chan struct{}) {
+func (server *superserver) Stop() {
+	close(server.stopListening)
+	server.listenerGroup.Wait()
+	<-server.supervisor.Exit()
+}
+
+func (server *superserver) listenForServiceConnections(config serviceConfig) {
 	addrString := ":" + strconv.Itoa(config.Port)
 	addr, err := net.ResolveTCPAddr("tcp", addrString)
 	if err != nil {
@@ -82,12 +64,12 @@ func listen(supervisor *Supervisor, config serviceConfig, stopListening <-chan s
 
 	for {
 		select {
-		case <-stopListening:
+		case <-server.stopListening:
 			return
 		default:
 		}
 
-		listener.SetDeadline(time.Now().Add(ListenerTimeout))
+		listener.SetDeadline(time.Now().Add(listenerTimeout))
 		conn, err := listener.Accept()
 		if err != nil {
 			if netErr, ok := err.(*net.OpError); ok && netErr.Timeout() {
@@ -100,7 +82,7 @@ func listen(supervisor *Supervisor, config serviceConfig, stopListening <-chan s
 
 		log.Printf("Connection has been accepted. Starting child service.\n")
 		go func() {
-			if err := supervisor.StartService(conn, config); err != nil {
+			if err := server.supervisor.StartService(conn, config); err != nil {
 				conn.Close()
 				log.Printf("Error starting service %s: %v\n", config.Name, err)
 			}
